24e53080a320ee8fb97eb6168755f8cd7995355d
daniel_nielsen <dn@mapspeople.com>
2015-09-11 23:05:12 +0000

    first version

---
+++ app.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var http = require('http');
+var lwip = require("lwip");
+
+app.get('/', function(req, res) {
+    req.query.layer = req.query.layer || [];
+    req.query.tint = req.query.tint || [];
+    var layers = req.query.layer.push ? req.query.layer : [req.query.layer];
+    var tints = req.query.tint.push ? req.query.tint : [req.query.tint];
+
+    renderResult(null, layers, tints, 0, res);
+
+});
+
+var server = app.listen(process.env.PORT, function() {
+    var host = process.env.IP;
+    var port = process.env.PORT;
+
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+var renderResult = function(masterImage, layers, tints, layerIndex, res) {
+    if (layerIndex > layers.length - 1) {
+        masterImage.toBuffer('png', function(err, buffer) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'image/png'
+            });
+            res.end(buffer, 'binary');
+        });
+        return;
+    }
+
+    var uri = layers[layerIndex];
+
+    http.get(uri, function(response) {
+
+        var imagedata = ''
+        response.setEncoding('binary')
+
+        response.on('data', function(chunk) {
+            imagedata += chunk
+        })
+
+        response.on('end', function() {
+            //if (err) {
+            //  res.send(err);
+            //}
+            lwip.open(new Buffer(imagedata, 'binary'), 'png', function(err, image) {
+                if (err) {
+                    res.send(err);
+                }
+
+                if (tints && tints[layerIndex]) {
+                    var tint = tints[layerIndex];
+                    (function setTint(x, y) {
+                        var color = image.getPixel(x, y);
+                        var tintColor = hexToRgb(tint)
+                        tintColor.a = color.a;
+                        image.setPixel(x, y, tintColor, function() {
+                            if (++x < image.width())
+                                setTint(x, y);
+                            else if (++y < image.height())
+                                setTint(0, y);
+                            else collapseImage();
+                        });
+    
+                    })(0, 0);
+                } else {
+                    collapseImage();
+                }
+
+                function collapseImage() {
+                    if (masterImage) {
+                        masterImage.paste(0, 0, image, function(err, image) {
+                            if (err) {
+                                res.send(err);
+                            }
+                            masterImage = image;
+
+                            renderResult(masterImage, layers, tints, ++layerIndex, res);
+
+                        });
+                    }
+                    else {
+                        masterImage = image;
+
+                        renderResult(masterImage, layers, tints, ++layerIndex, res);
+
+                    }
+                }
+            });
+        });
+    });
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
